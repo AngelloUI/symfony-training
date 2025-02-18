8cf67e672ebcd82c7c8bedbe53768c2c4f2db55b
Dmitry <usefullforit@gmail.com>
2025-02-18 21:32:38 +0300

    Testing debug-tool - dump

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -9,14 +9,14 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class ConferenceController extends AbstractController
 {
-    #[Route('/hello/{name}', name: 'homepage')]
-    public function index(string $name = ''): Response
+    #[Route('/', name: 'homepage')]
+    public function index(Request $request): Response
     {
+        dump($request);
         $greet = '';
-        if ($name) {
+        if ($name = $request->query->get('hello')) {
             $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
         }
-
         return new Response(<<<EOF
             <html>
                 <body>
